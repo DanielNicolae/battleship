e31ace3497069f82ff8bf052ac8ef6abc1743a20
DanielNicolae <deadlypoet@protonmail.com>
2021-07-30 20:19:09 +0300

    Added the generateGrids function.

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
-const battleship = require("battleship");
+// const battleship = require("battleship");
 
+function generateGrids() {
+  const yourGrid = document.getElementsByClassName("yourGrid")[0];
+  const computersGrid = document.getElementsByClassName("computersGrid")[0];
+  console.log(computersGrid)
+  for (let i = 0; i < 30; i++) {
+    for (let j = 0; j < 30; j++) {
+      const cell = document.createElement("div");
+      cell.classList.add("cell");
+      yourGrid.appendChild(cell);
+      computersGrid.appendChild(cell);
+    }
+  }
+}
+
+generateGrids();
