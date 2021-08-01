658054eef58df38de859608b42ea64e2d6de9c10
DanielNicolae <deadlypoet@protonmail.com>
2021-08-01 20:13:38 +0300

    I solved the bug regardind the display of the right colors on the grid and the bug regarding the absence of the second grid; I also added a title for each grid.

--- app.js
+++ app.js
@@ -3,13 +3,35 @@
 function generateGrids() {
   const yourGrid = document.getElementsByClassName("yourGrid")[0];
   const computersGrid = document.getElementsByClassName("computersGrid")[0];
-  console.log(computersGrid)
+  console.log(yourGrid);
   for (let i = 0; i < 30; i++) {
     for (let j = 0; j < 30; j++) {
-      const cell = document.createElement("div");
-      cell.classList.add("cell");
-      yourGrid.appendChild(cell);
-      computersGrid.appendChild(cell);
+      const cell1 = document.createElement("div");
+      const cell2 = document.createElement("div");
+      const cell3 = document.createElement("div");
+      const cell4 = document.createElement("div");
+      cell1.classList.add("cell1");
+      cell2.classList.add("cell2");
+      cell3.classList.add("cell1");
+      cell4.classList.add("cell2");
+      if (i % 2 === 0) {
+        if (i + j % 2 - i > 0) {
+          yourGrid.appendChild(cell1);
+          computersGrid.appendChild(cell3);
+        } else {
+          yourGrid.appendChild(cell2);
+          computersGrid.appendChild(cell4);
+        }
+      } else {
+        if (i + j % 2 - i > 0) {
+          yourGrid.appendChild(cell2);
+          computersGrid.appendChild(cell4);
+        } else {
+          yourGrid.appendChild(cell1);
+          computersGrid.appendChild(cell3);
+        }
+      }
+
     }
   }
 }
